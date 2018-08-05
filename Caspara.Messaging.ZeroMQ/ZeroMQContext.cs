202db6c36d8d0f3e785b462a4d99a79f41a340f0
Bjørn Esaiassen <bfe@icsys.no>
2018-08-05 04:04:01 +0200

    Changed structure of ZeroMQContext

--- Caspara.Messaging.ZeroMQ/ZeroMQContext.cs
+++ Caspara.Messaging.ZeroMQ/ZeroMQContext.cs
@@ -10,8 +10,8 @@ namespace Caspara.Messaging.ZeroMQ
     {
         private string SERVER_PUBLICKEY = "583D5D396B4F43353157777B7D253F6C25535757517D28506A2856347423616D396E6C4A59425D28";
         private string SERVER_PRIVATEKEY = "4A5E3F7A304F5A4C4B554450585646384444584444263A5A2976232875446976486C4C3F5235254F";
-        private byte[] ServerPublicKey => StringToByteArray(SERVER_PUBLICKEY);
-        private byte[] ServerPrivateKey => StringToByteArray(SERVER_PRIVATEKEY);
+        private byte[] ServerPublicKey => SERVER_PUBLICKEY.HexStringToByteArray();
+        private byte[] ServerPrivateKey => SERVER_PRIVATEKEY.HexStringToByteArray();
 
 
         List<ZSocket> Sockets = new List<ZSocket>();
@@ -21,27 +21,34 @@ namespace Caspara.Messaging.ZeroMQ
             context = new ZContext();
         }
 
-        public ZSocket CreateClientSocket(ZSocketType Type)
+        private ZSocket CreateSocket(ZSocketType Type)
         {
             var s = new ZSocket(context, Type);
             s.SetOption(ZSocketOption.CONFLATE, 1);
+            Sockets.Add(s);
+            return s;
+        }
+
+        public ZSocket CreateClientSocket(ZSocketType Type)
+        {
+            var s = CreateSocket(Type);
+
             Z85.CurveKeypair(out byte[] publicKey, out byte[] privateKey);
             s.CurvePublicKey = publicKey;
             s.CurveSecretKey = privateKey;
             s.CurveServerKey = ServerPublicKey;
-            Sockets.Add(s);
+
             return s;
         }
 
         public ZSocket CreateServerSocket(ZSocketType Type)
         {
-            var s = new ZSocket(context, Type);
-            s.SetOption(ZSocketOption.CONFLATE, 1);
-            Z85.CurveKeypair(out byte[] publicKey, out byte[] privateKey);
+            var s = CreateSocket(Type);
+
             s.CurveServer = true;
             s.CurvePublicKey = ServerPublicKey;
             s.CurveSecretKey = ServerPrivateKey;
-            Sockets.Add(s);
+            
             return s;
         }
 
@@ -56,14 +63,6 @@ namespace Caspara.Messaging.ZeroMQ
             context.Shutdown();
             context.Terminate();
             context.Dispose();
-        }
-
-        private byte[] StringToByteArray(string hex)
-        {
-            return Enumerable.Range(0, hex.Length)
-                             .Where(x => x % 2 == 0)
-                             .Select(x => Convert.ToByte(hex.Substring(x, 2), 16))
-                             .ToArray();
         }
 
         
